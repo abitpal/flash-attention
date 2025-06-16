aad7e953725847cb78f11f135fecf2db30031d93
pauldbd <loldeadmemegod@gmail.com>
2025-06-16 23:25:31 +0000

    Cleaned up unused vars

--- src/utils.cu
+++ src/utils.cu
@@ -18,7 +18,6 @@ Making sure we have memory coalescing within warps
 __device__ void _load_tile(float* source, float* target, int n, int start_row, int end_row, int start_col, int end_col, int thread_idx, int thread_cnt) {
     int n_cols = end_col - start_col + 1; 
     int n_rows = end_row - start_row + 1; 
-    int ld_amt_per_thread = (n_cols + thread_cnt - 1) / thread_cnt; 
     for (int i = 0; i < n_rows; i++) {
         for (int j = thread_idx; j < n_cols; j += thread_cnt) {
             int idx = i * n_cols + j; 
@@ -38,7 +37,7 @@ __device__ void _matmul_softmax(
         float dot_prod[max_queries_per_thread];  
         for (int j = 0; j < queries_per_thread; j++) dot_prod[j] = 0.0f; 
         for (int j = 0; j < d_k; j++) {
-            float k_val = k[n_seq_k * j + i]; 
+            float k_val = k[n_seq_k * j + i];
             for (int k = thread_idx, section = 0; k < n_seq_q; k += thread_cnt, ++section) {
                 float q_val = q[n_seq_q * j + k]; 
                 dot_prod[section] += k_val * q_val; 
@@ -59,7 +58,6 @@ __device__ void _matmul_softmax(
 }
 
 __device__ void _fill(float* arr, int size, float fill_val, int thread_cnt, int thread_idx) {
-    int val_per_thread = (size + thread_cnt - 1) / thread_cnt; 
     for (int i = thread_idx; i < size; i += thread_cnt) {
         arr[i] = fill_val; 
     }
